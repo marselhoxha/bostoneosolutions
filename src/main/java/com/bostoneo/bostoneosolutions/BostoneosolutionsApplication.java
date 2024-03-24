ff93d8fd91fcb90502ed12e1e805d480f68e6092
Marsel Hoxha <marsel.hox@gmail.com>
2024-03-24 17:26:31 -0400

    first commit

---
+++ src/main/java/com/bostoneo/bostoneosolutions/BostoneosolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.bostoneo.bostoneosolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BostoneosolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BostoneosolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bostoneo/bostoneosolutions/BostoneosolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bostoneo.bostoneosolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BostoneosolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
