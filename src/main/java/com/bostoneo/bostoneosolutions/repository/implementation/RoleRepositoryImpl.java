6ac73d0e68caa00932d6de7f4148e6e19ccf202d
Marsel Hoxha <marsel.hox@gmail.com>
2024-03-27 16:07:48 -0400

    Added .yml profiles for dev and prod

--- src/main/java/com/bostoneo/bostoneosolutions/repository/implementation/RoleRepositoryImpl.java
+++ src/main/java/com/bostoneo/bostoneosolutions/repository/implementation/RoleRepositoryImpl.java
@@ -53,7 +53,7 @@ public class RoleRepositoryImpl implements RoleRepository<Role> {
     public void addRoleToUser(Long userId, String roleName) {
         log.info("Adding role {} to user id: {}",roleName, userId);
         try{
-            Role role =jdbc.queryForObject(SELECT_ROLE_BY_NAME_QUERY, Map.of("roleName", roleName), new RoleRowMapper());
+            Role role =jdbc.queryForObject(SELECT_ROLE_BY_NAME_QUERY, Map.of("name", roleName), new RoleRowMapper());
             jdbc.update(INSERT_ROLE_TO_USER_QUERY, Map.of("userId", userId, "roleId", requireNonNull(role).getId()));
 
         }catch (EmptyResultDataAccessException exception) {
